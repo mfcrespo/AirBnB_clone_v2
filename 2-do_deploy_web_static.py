82e63a9d68727b99231d9fe9e18e248d4175acfe
mfcrespo <mafe_crespo@hotmail.com>
2020-08-18 04:38:12 +0000

    Write a Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ web_static folder of your AirBnB Clone repo, using the function do_pack
 """
 from fabric.api import *
 from datetime import datetime
-import os
+from os.path import isfile
 
 env.hosts = ['35.243.243.216', '54.146.219.183']
 
@@ -32,15 +32,15 @@ def do_deploy(archive_path):
     Write a Fabric script (based on the file 1-pack_web_static.py) that
     distributes an archive to your web servers, using the function do_deploy
     """
-    archive_path = "versions/web_static_20170315003959.tgz"
-    filename = archive_path.split("/")[1]
-    filename1 = (archive_path.split("/")[1]).split(".")[0]
-    input_path = "/data/web_static/releases/{}/".format(filename1)
 
-    if os.path.exists(archive_path) is False:
+    if isfile(archive_path) is False:
         return False
 
     try:
+        archive_path = "versions/web_static_20200818015236.tgz"
+        filename = archive_path.split("/")[1]
+        filename1 = (archive_path.split("/")[1]).split(".")[0]
+        input_path = "/data/web_static/releases/{}/".format(filename1)
         put(archive_path, "/tmp/")
         run("sudo mkdir -p {}".format(input_path))
         run("sudo tar -zxvf /tmp/{} -C {}".format(filename, input_path))
@@ -51,5 +51,5 @@ def do_deploy(archive_path):
         run("sudo ln -s {} /data/web_static/current".format(input_path))
         return True
 
-    except:
+    except Exception:
         return False
